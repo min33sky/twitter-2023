2b078fd26474b75635e35cf84d95c6bf4acf5f01
min33sky <min33sky@naver.com>
2023-04-16 07:47:45 +0900

    sibal

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -28,10 +28,9 @@ async function getUser(userId: string) {
   //? 캐시때문에 수정해도 반영이 안되므로 캐시를 무효화시키는 방법을 사용해야함
   const res = await fetch(`${process.env.BASE_URL}/api/users/${userId}`, {
     headers: {
-      'Content-Type': 'application/json',
       Accept: 'application/json',
     },
-    cache: 'no-store',
+    // cache: 'no-store',
   });
 
   console.log('### 중꺾마 : ', res);
