14fffe966ee4826ae8950bebc53ea00432442f35
min33sky <min33sky@naver.com>
2023-04-16 07:19:23 +0900

    plz

--- src/app/api/current/route.ts
+++ src/app/api/current/route.ts
@@ -3,29 +3,40 @@ import { prisma } from '@/lib/prismaDB';
 import { NextRequest, NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 import { getServerSession } from 'next-auth/next';
-import { getToken } from 'next-auth/jwt';
+import { decode } from 'next-auth/jwt';
 
 export async function GET(request: NextRequest) {
-  const session: any = await getToken({
-    req: request,
-    secret: process.env.NEXTAUTH_SECRET,
+  // const session: any = await getToken({
+  //   req: request,
+  //   secret: process.env.NEXTAUTH_SECRET,
+  // });
+
+  // console.log('### [GET] /api/current - session : ', session);
+
+  const nextCookies = cookies();
+
+  // key/value 배열 형태로 되어 있으므로 reduce를 사용해서 문자열로 만든다.
+  const cookie = nextCookies.getAll().reduce((acc, cur) => {
+    // return `${acc}${cur.name}=${cur.value}; `;
+    if (cur.name === 'next-auth.session-token') {
+      return acc + cur.value;
+    }
+
+    return acc;
+  }, '');
+
+  console.log('### [GET] /api/current  - cookies : ', cookie);
+
+  const decoded = await decode({
+    token: cookie,
+    secret: process.env.NEXTAUTH_SECRET!,
   });
-
-  console.log('### [GET] /api/current - session : ', session);
-
-  // const nextCookies = cookies();
-
-  // // key/value 배열 형태로 되어 있으므로 reduce를 사용해서 문자열로 만든다.
-  // const cookie = nextCookies.getAll().reduce((acc, cur) => {
-  //   return `${acc}${cur.name}=${cur.value}; `;
-  // }, '');
-
-  // console.log('### [GET] /api/current  - cookies : ', cookie);
+  console.log('### [GET] /api/current  - decoded 토큰 : ', decoded);
   // console.log('### [GET] /api/current  - authOptions : ', authOptions);
 
-  const session_TEMP = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-  console.log('### [GET] /api/current - session_TEMP : ', session_TEMP);
+  console.log('### [GET] /api/current - session : ', session);
 
   // console.log('### [GET] /api/current - session : ', session);
 
@@ -43,8 +54,8 @@ export async function GET(request: NextRequest) {
   try {
     const currentUser = await prisma.user.findUnique({
       where: {
-        // email: session.user!.email!,
-        email: session.email!,
+        email: session.user!.email!,
+        // email: session.email!,
       },
     });
 
