9a3f71962f7c5e648b651926a9664f8086c21500
min33sky <min33sky@naver.com>
2023-04-16 07:38:36 +0900

    asdfasdf

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -27,9 +27,18 @@ export default async function UserDetail({ params: { userId } }: Props) {
 async function getUser(userId: string) {
   //? 캐시때문에 수정해도 반영이 안되므로 캐시를 무효화시키는 방법을 사용해야함
   const res = await fetch(`${process.env.BASE_URL}/api/users/${userId}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
     cache: 'no-store',
   });
+
+  console.log('### 중꺾마 : ', res);
+
   const user: UserDetail = await res.json();
+
+  console.log('### user : ', user);
 
   return user;
 }
